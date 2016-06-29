fec4ec183da501cfdb37ead2cff1c9f63e55d1ef
sachin lamba <sachin.lamba@moldedbits.com>
2016-06-29 10:30:52 +0530

    Saving Altered photos in album.

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
     }
     
     @IBAction func save(sender: AnyObject) {
-        
+        UIImageWriteToSavedPhotosAlbum(imageView.image!, self, "image:didFinishSavingWithError:contextInfo:", nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -112,7 +112,17 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
         applyProcessing()
     }
     
-    
+    func image(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafePointer<Void>) {
+        if error == nil {
+            let ac = UIAlertController(title: "Saved!", message: "Your Filtered image has been saved to your photos.", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+        } else {
+            let ac = UIAlertController(title: "Save error", message: error?.localizedDescription, preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+        }
+    }
     
     
 }
